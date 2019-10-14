c9f0b14ae29a71a95a85063bb20deac82b6811ae
Salim Hartono <31237496+salimhartono@users.noreply.github.com>
2019-10-14 09:21:45 +0700

    refactoring code in MainActivty
    use clean code to make it easier for other people to understand and easier to maintenance

--- app/src/main/java/com/fadhlan/caculator/MainActivity.java
+++ app/src/main/java/com/fadhlan/caculator/MainActivity.java
@@ -12,9 +12,10 @@ public class MainActivity extends AppCompatActivity {
 
 
     // TODO 1 Deklarasi View
-    EditText nilai1 , nilai2;
-    TextView hasil;
-    Button tambah , kurang , Bagi , kali , clear;
+    //using specefic declaration for easy mainten
+    EditText etNilai1 , etNilai2;
+    TextView tvHasil;
+    Button btTambah , btKurang , btBagi , btKali , btClear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,23 +28,24 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        nilai1  = findViewById(R.id.nilai1);
-        nilai2  = findViewById(R.id.nilai2);
-        hasil   = findViewById(R.id.hasil);
-        tambah  = findViewById(R.id.tambah);
-        kali    = findViewById(R.id.kali);
-        kurang   = findViewById(R.id.kurang);
-        Bagi  = findViewById(R.id.bagi);
-        clear = findViewById(R.id.clear);
+        etNilai1  = findViewById(R.id.nilai1);
+        etNilai2  = findViewById(R.id.nilai2);
+        tvHasil   = findViewById(R.id.hasil);
+        btTambah  = findViewById(R.id.tambah);
+        btKali    = findViewById(R.id.kali);
+        btKurang   = findViewById(R.id.kurang);
+        btBagi  = findViewById(R.id.bagi);
+        btClear = findViewById(R.id.clear);
 
 
         // TODO 8 atur button untuk masing - masing operator
-        tambah.setOnClickListener(new View.OnClickListener() {
+        btTambah.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if (nilai1.getText().toString().isEmpty() || nilai2.getText().toString().isEmpty()){
-                    Toast.makeText(MainActivity.this, "ISI BRO", Toast.LENGTH_SHORT).show();
+                if (etNilai1.getText().toString().isEmpty() || etNilai2.getText().toString().isEmpty()){
+                    //
+                    Toast.makeText(MainActivity.this, "field can't be empty", Toast.LENGTH_SHORT).show();
                 }else {
                     tambah();
                 }
@@ -51,12 +53,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        kali.setOnClickListener(new View.OnClickListener() {
+        btKali.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if (nilai1.getText().toString().isEmpty() || nilai2.getText().toString().isEmpty()){
-                    Toast.makeText(MainActivity.this, "ISI BRO", Toast.LENGTH_SHORT).show();
+                if (etNilai1.getText().toString().isEmpty() || etNilai2.getText().toString().isEmpty()){
+                    Toast.makeText(MainActivity.this, "field can't be empty", Toast.LENGTH_SHORT).show();
                 }else {
                     kali();
                 }
@@ -64,12 +66,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Bagi.setOnClickListener(new View.OnClickListener() {
+        btBagi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if (nilai1.getText().toString().isEmpty() || nilai2.getText().toString().isEmpty()){
-                    Toast.makeText(MainActivity.this, "ISI BRO", Toast.LENGTH_SHORT).show();
+                if (etNilai1.getText().toString().isEmpty() || etNilai2.getText().toString().isEmpty()){
+                    Toast.makeText(MainActivity.this, "field can't be empty", Toast.LENGTH_SHORT).show();
                 }else {
                     Bagi();
                 }
@@ -77,12 +79,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        kurang.setOnClickListener(new View.OnClickListener() {
+        btKurang.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if (nilai1.getText().toString().isEmpty() || nilai2.getText().toString().isEmpty()){
-                    Toast.makeText(MainActivity.this, "ISI BRO", Toast.LENGTH_SHORT).show();
+                if (etNilai1.getText().toString().isEmpty() || etNilai2.getText().toString().isEmpty()){
+                    Toast.makeText(MainActivity.this, "field can't be empty", Toast.LENGTH_SHORT).show();
                 }else {
                     kurang();
                 }
@@ -91,11 +93,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        clear.setOnClickListener(new View.OnClickListener() {
+        btClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (nilai1.getText().toString().isEmpty() || nilai2.getText().toString().isEmpty()){
-                    Toast.makeText(MainActivity.this, "ISI BRO", Toast.LENGTH_SHORT).show();
+                if (etNilai1.getText().toString().isEmpty() || etNilai2.getText().toString().isEmpty()){
+                    Toast.makeText(MainActivity.this, "field can't be empty", Toast.LENGTH_SHORT).show();
                 }else {
                     clear();
                 }
@@ -105,63 +107,41 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
-
-
-
     }
+    
+    //gunakan jenis private apabila method hanya digunakan didalam kelas.
 
-
-
-
-
-
-
-
-
-
-
-    // TODO 3 Method tambah
-
-     void tambah(){
+     private void tambah(){
         // simpan inputan user kedalam variabel
-        int angka1 = Integer.parseInt(nilai1.getText().toString());
-        int angka2 = Integer.parseInt(nilai2.getText().toString());
-
+        int angka1 = Integer.parseInt(etNilai1.getText().toString());
+        int angka2 = Integer.parseInt(etNIlai2.getText().toString());
 
         // Cara Penjumlahan
         int total  = angka1 + angka2;
 
-        hasil.setText(String.valueOf(total));
-
-
+        tvHasil.setText(String.valueOf(total));
 
     }
 
     // TODO 4 Method kali
-
-
-     void kali(){
-         int angka1 = Integer.parseInt(nilai1.getText().toString());
-         int angka2 = Integer.parseInt(nilai2.getText().toString());
-
+     private void kali(){
+         int angka1 = Integer.parseInt(etNilai1.getText().toString());
+         int angka2 = Integer.parseInt(etNilai2.getText().toString());
 
          int total  = angka1 * angka2;
 
-         hasil.setText(String.valueOf(total));
+         tvHasil.setText(String.valueOf(total));
 
     }
 
     // TODO 5 Method kurang
-
-
-     void kurang(){
-         int angka1 = Integer.parseInt(nilai1.getText().toString());
-         int angka2 = Integer.parseInt(nilai2.getText().toString());
+     private void kurang(){
+         int angka1 = Integer.parseInt(etNilai1.getText().toString());
+         int angka2 = Integer.parseInt(etNilai2.getText().toString());
 
          int total  = angka1 - angka2;
 
-         hasil.setText(String.valueOf(total));
+         tvHasil.setText(String.valueOf(total));
 
 
      }
@@ -169,30 +149,25 @@ public class MainActivity extends AppCompatActivity {
     // TODO 6 Method bagi
 
 
-     void Bagi(){
-         double angka1 = Double.parseDouble(nilai1.getText().toString());
-         double angka2 = Double.parseDouble(nilai2.getText().toString());
+     private void Bagi(){
+         double angka1 = Double.parseDouble(etNilai1.getText().toString());
+         double angka2 = Double.parseDouble(etNilai2.getText().toString());
 
          double total  = angka1 / angka2;
 
-         hasil.setText(String.valueOf(total));
+         tvHasil.setText(String.valueOf(total));
 
 
      }
 
     // TODO 7 Method clear
+     private void clear(){
 
-
-     void clear(){
-
-        hasil.setText("");
-        nilai1.setText("");
-        nilai2.setText("");
+        tvHasil.setText("");
+        etNilai1.setText("");
+        etNilai2.setText("");
 
     }
-
-
-
 
 
 
